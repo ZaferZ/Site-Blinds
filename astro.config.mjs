d19895dc169950146374c09abea288c506fc94db
ZaferZ <zafer.bayraktarov@gmail.com>
2026-02-02 20:14:16 +0200

    Changed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://zaferz.github.io', 
   
-  base: '/Blinds',  
+  base: '/Site-Blinds',  
   
     vite: {
     plugins: [tailwindcss()]
