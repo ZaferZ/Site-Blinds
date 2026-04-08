a16fa0bfa2b862af3b8d9deafb33dc06428dd9e6
ZaferZ <zafer.bayraktarov@gmail.com>
2026-04-08 11:30:23 +0300

    Added base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'http://riohomes.bg', 
+  base: 'riohomes.bg/',
   
     vite: {
     plugins: [tailwindcss()]
